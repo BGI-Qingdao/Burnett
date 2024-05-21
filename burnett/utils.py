cf9e8d22849286bc12051df21e1defdccc016aa6
YaoLi3 <liyaoo6o@gmail.com>
2024-05-21 10:52:20 +0800

    add jaccard scores and heatmap

--- burnett/utils.py
+++ burnett/utils.py
@@ -60,3 +60,61 @@ def search_by_gene_symbol(fasta: dict, gene_symbol: str):
             matching_sequences[key] = value
     return matching_sequences
 
+
+def jaccard_score(df1, df2, cluster_col1, cluster_col2, protein_col):
+    import pandas as pd
+    jaccard_scores = []
+    # Iterate over each cluster in df1
+    for cluster1 in df1[cluster_col1].unique():
+        # Get proteins for the current cluster in df1
+        proteins_df1 = set(df1[df1[cluster_col1] == cluster1][protein_col])
+        # List to store similarity scores for current cluster
+        cluster_similarities = []
+        # Iterate over each cluster in df2
+        for cluster2 in df2[cluster_col2].unique():
+            # Get proteins for the current cluster in df2
+            proteins_df2 = set(df2[df2[cluster_col2] == cluster2][protein_col])
+            # Calculate Jaccard similarity
+            intersection_size = len(proteins_df1.intersection(proteins_df2))
+            union_size = len(proteins_df1.union(proteins_df2))
+            jaccard_score_value = intersection_size / union_size if union_size != 0 else 0
+            # Append similarity score to list
+            cluster_similarities.append((cluster2, jaccard_score_value))
+        # Sort cluster similarities based on similarity score
+        sorted_cluster_similarities = sorted(cluster_similarities, key=lambda x: x[1], reverse=False)
+        # Append all cluster similarities to jaccard_scores
+        for cluster2, similarity_score in sorted_cluster_similarities:
+            jaccard_scores.append([cluster1, cluster2, similarity_score])
+    return pd.DataFrame(jaccard_scores, columns=['Cluster1', 'Cluster2', 'Jaccard Similarity'])
+
+
+def jaccard_heatmap(jaccard_df):
+    import pandas as pd
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    # Pivot the DataFrame for plotting
+    heatmap_data = pd.pivot_table(jaccard_df, values='Jaccard Similarity', index=['Cluster1'], columns=['Cluster2'], sort=False)#, dropna=False)
+    heatmap_data = heatmap_data.iloc[:, ::-1]
+    # Create the heatmap
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(heatmap_data, annot=True, cmap="viridis", fmt=".2f", cbar=True)
+    plt.title('Jaccard Similarity Heatmap')
+    plt.xlabel('Clusters')
+    plt.ylabel('Clustering Output')
+    plt.savefig('jaccard_heatmap.png')
+
+
+def handle_cluster_output(cluster_output, ref):
+    """
+
+    :param cluster_output:
+    :param ref:
+    :return:
+    """
+    p1 = set(cluster_output.protein_ID)
+    p2 = set(ref.protein_ID)
+    non_p = p2 - p1
+    non_df = pd.DataFrame({'cluster_number': [-1] * len(non_p), 'protein_ID': list(non_p)})
+    cluster_output_with_non_cluster = pd.concat([cluster_output, non_df], ignore_index=True, sort=False)
+    return cluster_output_with_non_cluster
+
