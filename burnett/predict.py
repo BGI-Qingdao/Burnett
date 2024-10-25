ecf5ff4d87d35c3aae4b8ca71350a6e7cde4dcbc
YaoLi3 <liyaoo6o@gmail.com>
2024-10-25 09:45:24 +0800

    set max and min length for fasta seq; set chunk_size; save protein ids that could not be predicted by esmfold into a text file

--- burnett/predict.py
+++ burnett/predict.py
@@ -6,11 +6,12 @@
 import os
 import sys
 import torch
-# print(torch.__version__)  # 1.11.0+cu113
 torch.cuda.empty_cache()
+# print(torch.__version__)  # 1.11.0+cu113
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:32"
 import argparse
 import glob
+import time
 
 
 def format_pid_space(fasta_header, species=None):
@@ -27,11 +28,11 @@ def get_gene_id(fasta_header, species):
     pass
 
 
-def read_fasta(fasta_file, len_cutoff=0):
+def read_fasta(fasta_file, min_len=0, max_len=99999):
     """
     Read fa/fasta file
     :param fasta_file: protome fa file name
-    :param len_cutoff: amino acid sequence length cutoff (only seq longer than the len_cutoff will be kept)
+    :param min_len: amino acid sequence length cutoff (only seq longer than the len_cutoff will be kept)
     :return:
     """
     sequences = {}
@@ -41,14 +42,15 @@ def read_fasta(fasta_file, len_cutoff=0):
         for line in f:
             line = line.strip()
             if line.startswith(">"):  # header line
-                if current_sequence_id and len(current_sequence) >= len_cutoff:  # check if length > than cutoff
+                # check if max_len > length > than cutoff
+                if current_sequence_id and min_len <= len(current_sequence) <= max_len:
                     sequences[current_sequence_id] = current_sequence
                 current_sequence_id = line[1:]
                 current_sequence = ""
             else:  # sequence line
                 current_sequence += line
         # save last sequence (if meet length requirement)
-        if current_sequence_id and len(current_sequence) >= len_cutoff:
+        if current_sequence_id and len(current_sequence) >= min_len:
             sequences[current_sequence_id] = current_sequence
     return sequences
 
@@ -79,16 +81,19 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Predict protein structure via ESMFold",
                                      fromfile_prefix_chars="@",
                                      add_help=True,
-                                     epilog="Example: python predict.py -f test.fa -o /proj/results/ -s mouse --length 50"
+                                     epilog="Example: python predict.py -f test.fa -o /proj/results/ -s mouse "
+                                            "--min_len 50 --max_len 300 "
                                      )
     parser.add_argument('-f', '--fa', type=str, help='protein sequence fa file')
     parser.add_argument('-o', '--output', type=str, help='path to output pdb directory')
-    parser.add_argument('-i', '--input', type=str, default=None,
-                        help='[Optional] file of list of gene names to predict if applicable. '
-                             'Use all genes in the fa file when set to None')
+    # parser.add_argument('-i', '--input', type=str, default=None,
+    #                     help='[Optional] file of list of gene names to predict if applicable. '
+    #                          'Use all genes in the fa file when set to None')
     parser.add_argument('-s', '--species', type=str, default=None, help='[Optional] Species name if applicable')
-    parser.add_argument('--length', type=int, default=0,
+    parser.add_argument('--min_len', type=int, default=0,
                         help='[Optional] minimal length of a amino acid sequence to use')
+    parser.add_argument('--max_len', type=int, default=99999,
+                        help='[Optional] maximal length of a amino acid sequence to use')
     args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
 
     # create output directory
@@ -105,47 +110,52 @@ if __name__ == '__main__':
 
     # 0. Load in pretrained models
     model_path = '/home/share/huadjyin/home/fanguangyi/.cache/torch/hub/checkpoints/esmfold_3B_v1.pt'
-    # For esm2 pretrained models
-    # model, alphabet = esm.pretrained.load_model_and_alphabet(model_path)
     print('Loading pretrained model...')
     import esm.esmfold.v1.pretrained
     model = esm.esmfold.v1.pretrained._load_model(model_path)
     model = model.eval().cuda()  # class 'esm.esmfold.v1.esmfold.ESMFold'
 
+    # 2024-10-24: TODO: test this parameter. 0K de
+    model.set_chunk_size(8)
+    
+    print(torch.cuda.memory_summary())
+
     # 1. Load in protome data for one species
-    fasta = read_fasta(args.fa, len_cutoff=args.length)  # dictionary
+    fa_idx = os.path.basename(args.fa)
+    print(f'Running {fa_idx}')
+    fasta = read_fasta(args.fa, min_len=args.min_len, max_len=args.max_len)  # dictionary
     print(f'Load fasta file done, contain {len(fasta)} entries.')
 
     # 2. get sequences
-    if args.input:  # when user designate target genes/proteins to predict
-        with open(args.input, 'r') as f:
-            gene_list = f.read().splitlines()
-        for gene in gene_list:
-            pid, sequence = get_sequence(gene, fasta)
-            # 3. predict protein structures
-            print('Predicting...')
-            with torch.no_grad():
-                output = model.infer_pdb(sequence)  # string
-            print('Saving...')
-            with open(os.path.join(out_dir, '{}.pdb'.format(pid)), 'w') as f:
-                f.write(output)
-    else:  # when input gene_list file does not exist, use all genes/proteins in the fasta file
-        n = 0
-        x = 0
-        for pid, sequence in fasta.items():
-            reformated_pid = format_pid_space(pid)
-            if reformated_pid in protein_predicted:  # if the protein was predicted already
-                print('protein was predicted')
-                n += 1
-                continue
-            else:
+    n = 0
+    x = 0
+    error_proteins = set()
+    for pid, sequence in fasta.items():
+        reformated_pid = format_pid_space(pid)
+        if reformated_pid in protein_predicted:  # if the protein was predicted already
+            print('protein was predicted')
+            n += 1
+            continue
+        else:
+            try:
                 # 3. predict protein structures
                 print('Predicting...')
-                x += 1
+                torch.cuda.empty_cache()
+                start_time = time.time()
                 with torch.no_grad():
                     output = model.infer_pdb(sequence)  # string
+                end_time = time.time()
+                print(f'{round(end_time-start_time, 2)} seconds used.')
                 print('Saving...')
                 with open(os.path.join(out_dir, '{}.pdb'.format(reformated_pid)), 'w') as f:
                     f.write(output)
-        print(f'{n} proteins was predicted, {x} have not.')
+                x += 1
+            except RuntimeError:
+                error_proteins.add(reformated_pid)
+                print(f'OOM: {reformated_pid}, {len(sequence)}')
+                continue
+    print(f'{n} proteins were predicted before, {x} were predicted this time, {len(error_proteins)} failed.')
 
+    # save proteins id that were not successfully predicted
+    with open(os.path.join(out_dir, f'error_proteins_id_{fa_idx.strip(".fa")}.txt'), 'w') as f:
+        f.writelines('\n'.join(list(error_proteins)))
