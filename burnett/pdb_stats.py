7ae2e0b0a2f37786986e6d2c0e9bfe2013b68e48
YaoLi3 <liyaoo6o@gmail.com>
2024-11-01 15:57:46 +0800

    save low quality protein ids to file

--- burnett/pdb_stats.py
+++ burnett/pdb_stats.py
@@ -3,6 +3,7 @@
 # @Date: Created on 31 Oct 2024 10:14
 # @Author: Yao LI
 # @File: burnett/pdb_stats.py
+import os
 import sys
 
 import glob
@@ -47,12 +48,13 @@ def proportion_ppdlt(data, cutoff=70):
 
 def species_pdb_stats(species_dir):
     species_pdbs = glob.glob(f'{species_dir}/*.pdb')
-    total_mean_plddt = []  # plddt mean values
-    total_percet_plddt = []  # num of plddt values larger than cutoff / total plddt nums
+    total_mean_plddt = {}  # plddt mean values
+    total_percet_plddt = {} # num of plddt values larger than cutoff / total plddt nums
     for fn in species_pdbs:
         struct = read_pdb(fn)
-        total_mean_plddt.append(mean_ppdlt(struct))
-        total_percet_plddt.append(proportion_ppdlt(struct))
+        pid = os.path.basename(fn).strip('.pdb')
+        total_mean_plddt[pid] = mean_ppdlt(struct)
+        total_percet_plddt[pid] = proportion_ppdlt(struct)
     return total_percet_plddt, total_mean_plddt
 
 
@@ -60,8 +62,8 @@ if __name__ == '__main__':
     species_dir = sys.argv[1]
     p_list, m_list = species_pdb_stats(species_dir)
 
-    list_50 = [i for i in p_list if i >= 0.5]  # > 50 percent plddt is larger than 70
-    list_70 = [i for i in m_list if i >= 70]  # mean plddt value is larger than 70
+    list_50 = [k for k,v in p_list.items() if v >= 0.5]  # > 50 percent plddt is larger than 70
+    list_70 = [k for k,v in m_list.items() if v >= 70]  # mean plddt value is larger than 70
     print(f'{len(list_50)} out of {len(p_list)} proteins have more than 50% atom which plddt value is bigger than 70.')
     print(f'{len(list_70)} out of {len(m_list)} proteins have mean plddt value that is larger than 70.')
     with open(f'{species_dir}_low_plddt_percent.txt', 'w') as f:
@@ -70,14 +72,14 @@ if __name__ == '__main__':
         f.writelines('\n'.join(list_70))
 
     # plot histogram
-    plt.hist(p_list)
+    plt.hist(list(p_list.values()))
     plt.xlabel('percentage of pLDDT greater than 70')
     plt.ylabel('num of proteins')
     plt.title(f'{species_dir}')
     plt.savefig(f'{species_dir}_percent.png')
     plt.close()
 
-    plt.hist(m_list)
+    plt.hist(list(m_list.values()))
     plt.xlabel('mean pLDDT value')
     plt.ylabel('num of proteins')
     plt.title(f'{species_dir}')
